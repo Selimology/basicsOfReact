fbba0a81c9c6cb1028c54dec852e2b9e8105fb15
Kamil Ertekin <48670775+Selimology@users.noreply.github.com>
2022-05-19 02:47:44 +0200

    converted all useState to UseReducer learned more about dispatch, parameters, reducer function and switch.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./styles.css";
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 
 //define reducer (state,action)
+//...state is used so we can access all states and
+//overwrite the state we want specifically
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "increment":
-      return { count: state.count + 1 };
+      return { ...state, count: state.count + 1 };
     case "decrement":
-      return { count: state.count - 1 };
+      return { ...state, count: state.count - 1 };
+    case "newUserInput":
+      return { ...state, userInput: action.payload };
+    case "tgColor":
+      return { ...state, color: !state.color };
     default:
       throw new Error();
   }
@@ -16,20 +22,26 @@ const reducer = (state, action) => {
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, {
-    count: 0
+    count: 0,
+    userInput: "",
+    color: false
   });
 
   //Converting from useState to useReducer
-  const [userInput, setUserInput] = useState("");
-  // const [count, setCount] = useState(0);
-  const [color, setColor] = useState(false);
+  // const [userInput, setUserInput] = useState("");
+  // // const [count, setCount] = useState(0);
+  // const [color, setColor] = useState(false);
 
   return (
-    <main className="App" style={{ color: color ? "#FFF" : "#FFF952" }}>
+    <main className="App" style={{ color: state.color ? "#FFF" : "#FFF952" }}>
       <input
         type="text"
-        value={userInput}
-        onChange={(e) => setUserInput(e.target.value)}
+        value={state.userInput}
+        //we also need the parameter (target.value) so dispatch
+        //is slightly different
+        onChange={(e) =>
+          dispatch({ type: "newUserInput", payload: e.target.value })
+        }
       />
       <br />
       <br />
@@ -37,9 +49,9 @@ export default function App() {
       <section>
         <button onClick={() => dispatch({ type: "decrement" })}>-</button>
         <button onClick={() => dispatch({ type: "increment" })}>+</button>
-        <button onClick={() => setColor((prev) => !prev)}>Color</button>
+        <button onClick={() => dispatch({ type: "tgColor" })}>Color</button>
       </section>
-      <p>{userInput}</p>
+      <p>{state.userInput}</p>
     </main>
   );
 }
